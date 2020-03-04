8bdd8130c780200f932949e31cae07d044ce8463
mushuyun <mushuyun@yahoo.com>
2020-03-03 23:26:06 -0500

    uploading package

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# Readme-Generator
-Using command-line application to generate a readme.md file
+
+		# Project Title
+		Readme Generator 
+
+		## Project Description
+		Using command-line application to generate readme file
+
+		## Tableofcontents
+		Table of contents:
+		
+* [ProjectTitle](#ProjectTitle)
+		
+* [Author](#Author)
+		
+* [Installation](#ModulesInstalled)
+		
+* [Instructions](#Instructions)
+		
+* [License](#License)
+		
+* [Tests](#Tests)
+		
+* [Questions](#Questions)
+		
+		## Installation
+		1. axios 2.jest 3.util 4.inquirer
+		
+	![dependencyUptodate](./assets/deUpdate.png)
+
+		## Instructions
+		Follwoing command-line lead to enter answers
+
+		## License 
+		This project is licensed under the MIT 
+
+		## Tests
+		Runnin jest
+
+		## Questions
+		How to create better tests?
+
+		## Contributor 
+		
+	![ProfileImage](https://avatars3.githubusercontent.com/u/56772371?v=4)
+		
+	**mushuyun**
+			
+	Email: rtpice.mu@gmail.com
+			
+	Location: Research Triangle Park , NC
+			
+	GitHub: https://github.com/mushuyun
+		
---
+++ index.js
@@ -0,0 +1,126 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const util = require("util");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+async function generate(){
+	try{
+		console.log(`taking user input`);
+		const userInput = await inquirer
+		.prompt([
+			{
+				type: "input",
+				message: "What is your GitHub user name?",
+				name: "username"
+			},
+			{
+				type: "input",
+				message: "What is your Project Tittle?",
+				name: "projectTittle"
+			},
+			{
+				type: "input",
+				message: "Please describe your project",
+				name: "projectDescription"
+			},
+			{
+				type: "input",
+				message: "Provide table of contents",
+				name: "tableOfContents"
+			},
+			{
+				type: "input",
+				message: "What are the modules required to install for your project?",
+				name: "installationSteps"
+			},
+			{
+				type: "input",
+				message: "Provide instructions for use.",
+				name: "instruction"
+			},
+			{
+				type: "input",
+				message: "provide License name ",
+				name: "licenseName"
+			}, 
+			{
+				type: "input",
+				message: "Provide information on how to run tests.",
+				name: "tests"
+			},
+			{
+				type: "input",
+				message: "Do you have any questions?",
+				name: "questions"
+			}
+			]);
+			console.log(`generating...`);
+			console.log(userInput);
+			const gitUsername = userInput.username;
+			const projectTittle = userInput.projectTittle;
+			const projectDescription = userInput.projectDescription;
+			const installationSteps = userInput.installationSteps;
+			const instruction = userInput.instruction;
+			const tableOfContents = userInput.tableOfContents;
+			const licenseName = userInput.licenseName;
+			const tests = userInput.tests;
+			const questions = userInput.questions;
+				// fetching data from git user
+			const gitResponse = await axios.get(`https://api.github.com/users/${gitUsername}`);
+			const gitData = gitResponse.data;
+			const gitName = gitData.login;
+			const gitEmail = gitData.email;
+			const gitlocation = gitData.location;
+			const gitUrl = gitData.html_url;
+			const gitProfileImage = gitData.avatar_url;
+
+				var contents = (`
+		# Project Title
+		${projectTittle} 
+
+		## Project Description
+		${projectDescription}
+
+		## Tableofcontents
+		${tableOfContents}
+		\n* [ProjectTitle](#ProjectTitle)
+		\n* [Author](#Author)
+		\n* [Installation](#ModulesInstalled)
+		\n* [Instructions](#Instructions)
+		\n* [License](#License)
+		\n* [Tests](#Tests)
+		\n* [Questions](#Questions)
+		
+		## Installation
+		${installationSteps}
+		\n![dependencyUptodate](./assets/deUpdate.png)
+
+		## Instructions
+		${instruction}
+
+		## License 
+		This project is licensed under the ${licenseName} 
+
+		## Tests
+		${tests}
+
+		## Questions
+		${questions}
+
+		## Contributor 
+		\n![ProfileImage](${gitProfileImage})
+		\n**${gitName}**
+		\nEmail: ${gitEmail}
+		\nLocation:${gitlocation}
+		\nGitHub: ${gitUrl}
+		`)
+
+		await writeFileAsync('readMe.md', contents);
+		console.log("file generated....");
+	} 	catch (err){
+		console.log(err);
+		}	
+}	
+	generate();
